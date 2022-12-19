0b005e4d95e5e7c1ae03ae4a8f893ca25ddd9904
clenharta007 <40058498+clenharta007@users.noreply.github.com>
2022-12-19 19:33:24 +0700

    First Proj Build

---
+++ projAPIDapper/Context/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using projAPIDapper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projAPIDapper.Context
+{
+    public class MyContext:DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+
+        }
+        public DbSet<PresenceBook> PresenceBooks { get; set; }
+    }
+}
---
+++ projAPIDapper/Models/PresenceBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projAPIDapper.Models
+{
+    public class PresenceBook
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Provinsi { get; set; }
+        public string Kota { get; set; }
+        public string Kecamatan { get; set; }
+        public string Kelurahan { get; set; }
+        public string Kodepos { get; set; }
+        public DateTime DateAdded { get; set; }
+
+    }
+}
